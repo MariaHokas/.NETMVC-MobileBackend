42a6df95620d40ff052a07fd46777f4e9900e1f2
Ahokas Maria Katariina <maria.ahokas@stud.pointcollege.fi>
2019-03-20 19:54:09 +0200

    Added Chart -part2

---
+++ MobileBackend/Models/WorkAssigmentOperationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileFronend.Models
+{
+    public class WorkAssigmentOperationModel
+    {
+        public string Operation { get; set; }
+        public string AssignmentTitle { get; set; }
+    }
+}
